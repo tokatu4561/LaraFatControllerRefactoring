72b0e9b981d6b515464d92276a5c157245c04210
tokatu4561 <ryobsk4561@gmail.com>
2021-11-22 06:35:34 -0800

    create form request class for validate

--- src/app/Http/Controllers/Bookmarks/BookmarkController.php
+++ src/app/Http/Controllers/Bookmarks/BookmarkController.php
@@ -9,6 +9,7 @@ use App\Models\Bookmark;
 use App\Models\BookmarkCategory;
 use App\Models\User;
 use App\Bookmark\UseCase\ShowBookmarkListPageUseCase;
+use App\Http\Requests\CreateBookmarkRequest;
 use Artesaos\SEOTools\Facades\SEOTools;
 use Dusterio\LinkPreview\Client;
 use Dusterio\LinkPreview\Exceptions\UnknownParserException;
@@ -110,18 +111,12 @@ class BookmarkController extends Controller
      * @param Request $request
      * @return Application|\Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
      */
-    public function create(Request $request)
+    public function create(CreateBookmarkRequest $request)
     {
         if (Auth::guest()) {
             // @note ここの処理はユーザープロフィールでも使われている
             return redirect('/login');
         }
-
-        Validator::make($request->all(), [
-            'url' => 'required|string|url',
-            'comment' => 'required|string|min:10|max:1000',
-            'category' => 'required|integer|exists:bookmark_categories,id',
-        ])->validate();
 
         // 下記のサービスでも同様のことが実現できる
         // @see https://www.linkpreview.net/
